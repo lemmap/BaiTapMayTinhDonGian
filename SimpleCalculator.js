af90247fc22d7c84ee883096568d011f3d46e281
cuckixauxa@gmail.com <Lemmap321>
2020-03-19 20:35:39 +0700

    finnish

--- SimpleCalculator.js
+++ SimpleCalculator.js
@@ -1,3 +1,98 @@
-function clickBtn1(value){
-    document.getElementById("inputValue").innerHTML="value";
+function clickBtn1(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    element.value+=value;
+}
+function clickBtn2(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtn3(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtn4(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtn5(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtn6(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtn7(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtn8(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtn9(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtn0(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtnmulti(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtndivi(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtnplus(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtnminus(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value+=value;
+}
+function clickBtndelete(value) {
+    console.log(value);
+    let element=document.getElementById("displayInput");
+    // console.log(element.value)
+    element.value="";
+    console.log(element.value)
+}
+function clickBtnequal(){
+    let element=document.getElementById("displayInput");
+
+    let result= eval(element.value);
+    element.value=result;
+
+    console.log(result);
 }
